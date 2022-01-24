bf4ef19e95fc2cf4237a13233f818b922e307934
Marquel Rogers <69293041+msrogers2015@users.noreply.github.com>
2022-01-24 17:51:07 -0600

    Update main.py
    Rewrote info function to work with a cog-less script. Init was thrown together in a rush. Code needs to be tested.

--- main.py
+++ main.py
@@ -8,14 +8,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-bot = commands.Bot(command_prefix='...')
+bot = commands.Bot(command_prefix='covid')
 bot.remove_command('help')
 
 
 @bot.event
 async def on_ready():
 	print('Bot is online')
+	
+@bot.command()
+async def ping(ctx):
+    embed = discord.Embed(title="Pong!", description=f'{round(bot.latency*1000)}ms', color=embed_color)
+    await ctx.send(embed=embed)
+	
+@bot.command()
+async def info(ctx, *, location)
+	db = await aiosqlite.connect('./covid.db')
+	cursor = await db.execute("SELECT * FROM countries WHERE Country='United States of America'")
+	row = await cursor.fetchone()
+	print(row)
+	await db.close()
 
+
+'''Fix this section later, "blind coding" at work	
 @commands.command()
 async def get_record(self, ctx):
 	db = await aiosqlite.connect('/home/mrogers/Documents/Fiverr_Template/cogs/covid.db')
@@ -26,6 +41,6 @@ async def get_record(self, ctx):
 #@tasks.loop(seconds=2.0)
 #async def foo():
 #	print('This is a repeat task')
-#foo.start()
+#foo.start()'''
 
 bot.run(TOKEN)
