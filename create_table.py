3967c247384e5b5b994f24bde8726305bd3153d2
Marquel Rogers <69293041+msrogers2015@users.noreply.github.com>
2022-01-25 22:45:45 -0600

    Create create_table.py
    Quick script to recreate the database.

---
+++ create_table.py
@@ -0,0 +1,14 @@
+def create_database():
+	conn = sqlite3.connect('./covid.db')
+	cur = conn.cursor()
+	cur.execute('''CREATE TABLE countries(
+			Country TEXT PRIMARY KEY,
+			NewConfirmed INTEGER,
+			TotalConfirmed INTEGER,
+			NewDeaths INTEGER,
+			TotalDeaths INTEGER,
+			NewRecovered INTEGER,
+			TotalRecovered INTEGER);'''
+	conn.commit()
+	conn.close()
+	print('Database Created')
